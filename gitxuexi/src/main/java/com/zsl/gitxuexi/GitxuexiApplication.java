4c4bf1f6160f73f324b9b9f78f763c9663a3ea35
zsl146036 <987221583@qq.com>
2020-07-22 10:26:22 +0800

    第一次提交信息

---
+++ gitxuexi/src/main/java/com/zsl/gitxuexi/GitxuexiApplication.java
@@ -0,0 +1,13 @@
+package com.zsl.gitxuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitxuexiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitxuexiApplication.class, args);
+    }
+
+}
